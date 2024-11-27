04ae00e8b1128c40055c6282f581ada5380e45a6
Taner Saydam <83103843+TanerSaydam@users.noreply.github.com>
2024-11-27 17:55:31 +0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,83 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ElementRef, signal, input, output, viewChild, computed, linkedSignal, effect, Injector } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from "./home/home.component";
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+declare global {
+  var apiKey: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [FormsModule, HomeComponent, CommonModule],  
+  template: `
+  <h1 #h1el>Angular v19</h1>
+  <input #myinput [(ngModel)]="name"/>
+  <input [(ngModel)]="userId"/>
+
+  @let nameInputVar = "Hello " +  myinput.value;
+
+  <p>{{nameInputVar}}</p>  
+
+  @let user = user$ | async;
+  <p>{{user.username}}</p>
+  <button (click)="method()">call method</button>
+  <!-- <p>{{value()}}</p> -->
+  <!-- <p>{{fullNameLinked()}}</p> -->
+  <app-home [userId]="userId()"></app-home>
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-app';
+  name = signal<string>(""); 
+  userId = signal<number>(1);
+  user$!: Observable<any>;
+
+  fullName = computed(()=> {
+    //işlemler
+    return "Bay " + this.name();
+  });
+
+  // fullNameLinked = linkedSignal(()=> {
+  //   //işlemler
+  //   return "Bay " + this.name();
+  // })
+
+  // fullNameLinked = linkedSignal({
+  //   source: this.name,
+  //   computation: ((newVersion, prevVersion)=>{
+  //     console.log("New Version: " + newVersion);
+  //     console.log("Old Version: " + prevVersion?.value);
+  //     return "... " + this.name();
+  //   })
+  // })
+  
+  readonly value = input<string>("");
+  readonly event = output<string>();
+  readonly h1el = viewChild.required<ElementRef<HTMLHeadingElement>>("h1el");
+
+  constructor(private injector: Injector, private http: HttpClient){
+
+      this.user$ = this.http.get("https://jsonplaceholder.typicode.com/users/3");
+
+    //console.log("Api key: " + apiKey);
+    //this.fullNameLinked.set("Deneme");   
+    //this.value.set("Test"); //set edilemez
+  }
+
+  method(){
+    effect(()=> {
+      if(this.userId()){
+        console.log("detecting some changes in user Id...");
+      }      
+    },{injector: this.injector})
+
+    effect(()=> {
+      if(this.name()){
+        console.log("detecting some changes in name...");        
+      }
+    },{injector: this.injector})
+  }
 }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,42 @@
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Component, inject, input, resource, signal } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { lastValueFrom } from "rxjs";
+
+@Component({
+  selector: 'app-home',
+  imports:[FormsModule],
+  template: `
+  <h1>Home Component</h1>
+  <p>{{data.isLoading() ? 'Yükleniyor...' : 'Yüklendi'}}</p>
+  <input type="text" [(ngModel)]="userId2">
+  @if(!data.isLoading() && data.error() === undefined){
+    <p>{{data.value().name}}</p>
+    <p>{{data.value().username}}</p>
+    <p>{{data.value().email}}</p>
+  }
+  @if(data.error()){
+    <p>Error: {{showError()}}</p>
+  }
+  `,
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  userId = input.required<number>();
+  userId2 = signal<number>(1);
+  #http = inject(HttpClient);
+
+  data = resource({
+    request: this.userId2,
+    loader: async ({request: id}) => {
+      //işlemler
+      await new Promise((res)=> setTimeout(res, 1000));
+      const result = await lastValueFrom(this.#http.get<any>(`https://jsonplaceholder.typicode.com/users/${id}`));
+      return result;
+    }
+  })
+
+  showError(){
+   return (this.data.error() as HttpErrorResponse).message;
+  }
+}
